f55e525490fcc9dead9a7da3b3a21793e95590e4
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-12-17 11:50:16 +0200

    Remove database functions from route

--- routes.py
+++ routes.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import redirect, render_template, request
 from app import app
 from db import db
+import timemanager
 
 @app.route("/form")
 def form():
@@ -18,20 +19,12 @@ def create():
 def result():
     task = request.form["task"]
     time = request.form["time"]
-    # user_id is temporarily hardcoded
-    id = 1
-    sql_insert = "INSERT INTO time (hours, task, user_id, logged_at) VALUES (:time, :task, :id, NOW())"
-    sql_tot_time = "SELECT SUM (hours) FROM time WHERE user_id =:user_id"
-
-    db.session.execute(sql_insert, {"time":time, "task":task, "id":id})
-    db.session.commit()
-    print("debug print")
-    result = db.session.execute(sql_tot_time, {"user_id":id})
-    total = result.fetchone()[0]
-    db.session.commit()
-
+    
+    timemanager.add_timestamp(time, task, 1)
+    total_time = timemanager.get_total_time_user(1)
+    
     return render_template("result.html", task=task,
                                           time=time,
-                                          total=total,
+                                          total=total_time,
                                           user_id = id
                                           )
