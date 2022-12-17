c21c8b81e13c0e6cdad0bede5644ba12e1989332
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-12-17 17:38:40 +0200

    Autopep8 format and lint added

--- routes.py
+++ routes.py
@@ -1,14 +1,18 @@
 from flask import redirect, render_template, request, session
 from app import app
-import timemanager, users
+import timemanager
+import users
+
 
 @app.route("/form")
 def form():
     return render_template("form.html")
 
+
 @app.route("/create_user")
 def create_page():
     return render_template("create_user.html")
+
 
 @app.route("/create_user", methods=["POST"])
 def create_user():
@@ -16,36 +20,40 @@ def create_user():
     password = request.form["password"]
     password_repeat = request.form["password_repeat"]
 
-    if users.create_user(username, password) and password==password_repeat:
+    if users.create_user(username, password) and password == password_repeat:
         if users.login(username, password):
             session["username"] = username
             return redirect("/")
-        else:
-            return redirect("/login")
+        return redirect("/login")
     else:
         return redirect("/create_user")
 
+
 @app.route("/result", methods=["POST"])
 def result():
+    # TODO: token check
     task = request.form["task"]
     time = request.form["time"]
-    
+
     timemanager.add_timestamp(time, task, 1)
     total_time = timemanager.get_total_time_user(1)
 
     return render_template("result.html", task=task,
-                                          time=time,
-                                          total=total_time,
-                                          user_id = id
-                                          )
+                           time=time,
+                           total=total_time,
+                           user_id=id
+                           )
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/login")
 def show_login_field():
     return render_template("login.html")
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -56,6 +64,7 @@ def login():
         return redirect("/")
     else:
         return redirect("/login")
+
 
 @app.route("/logout")
 def logout():
--- timemanager.py
+++ timemanager.py
@@ -1,13 +1,15 @@
 from db import db
 
+
 def add_timestamp(time, task, user_id):
     sql_insert = "INSERT INTO time (hours, task, user_id, logged_at) VALUES (:time, :task, :id, NOW())"
-    db.session.execute(sql_insert, {"time":time, "task":task, "id":user_id})
+    db.session.execute(sql_insert, {"time": time, "task": task, "id": user_id})
     db.session.commit()
+
 
 def get_total_time_user(user_id):
     sql_tot_time = "SELECT SUM (hours) FROM time WHERE user_id =:user_id"
-    result = db.session.execute(sql_tot_time, {"user_id":user_id})
+    result = db.session.execute(sql_tot_time, {"user_id": user_id})
     total = result.fetchone()[0]
     db.session.commit()
     return total
